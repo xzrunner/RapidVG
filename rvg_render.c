95055eadc3345864621e382427725213f803b4d4
zz <zhuguang@ejoy.com>
2016-04-22 10:41:01 +0800

    rename

--- rvg_render.c
+++ rvg_render.c
@@ -16,30 +16,30 @@
 
 void 
 rvg_point(float x, float y) {
-	sl_shape_type(TYPE_POINTS);
+	sl_shape2_type(TYPE_POINTS);
 
 	float coords[2];
 	coords[0] = x;
 	coords[1] = y;
-	sl_shape_draw(coords, 1);
+	sl_shape2_draw(coords, 1);
 }
 
 void 
 rvg_line(float x0, float y0, float x1, float y1) {
-	sl_shape_type(TYPE_LINES);
+	sl_shape2_type(TYPE_LINES);
 	
 	float coords[4];
 	coords[0] = x0;
 	coords[1] = y0;
 	coords[2] = x1;
 	coords[3] = y1;
-	sl_shape_draw(coords, 2);	
+	sl_shape2_draw(coords, 2);	
 }
 
 void 
 rvg_lines(const float* positions, int count) {
-	sl_shape_type(TYPE_LINES);
-	sl_shape_draw(positions, count);
+	sl_shape2_type(TYPE_LINES);
+	sl_shape2_draw(positions, count);
 }
 
 void 
@@ -48,36 +48,36 @@ rvg_polyline(const float* positions, int count, bool loop) {
 		return;
 	}
 
-	sl_shape_type(TYPE_LINE_STRIP);
-	sl_shape_draw_node(positions[0], positions[1], true);
-	sl_shape_draw(positions, count);
+	sl_shape2_type(TYPE_LINE_STRIP);
+	sl_shape2_draw_node(positions[0], positions[1], true);
+	sl_shape2_draw(positions, count);
 	if (loop) {
-		sl_shape_draw_node(positions[0], positions[1], false);
-		sl_shape_draw_node(positions[0], positions[1], true);
+		sl_shape2_draw_node(positions[0], positions[1], false);
+		sl_shape2_draw_node(positions[0], positions[1], true);
 	} else {
-		sl_shape_draw_node(positions[(count - 1) * 2], positions[(count - 1) * 2 + 1], true);
+		sl_shape2_draw_node(positions[(count - 1) * 2], positions[(count - 1) * 2 + 1], true);
 	}
 }
 
 void 
 rvg_triangles(const float* positions, int count) {
-	sl_shape_type(TYPE_TRIANGLES);
-	sl_shape_draw(positions, count);
+	sl_shape2_type(TYPE_TRIANGLES);
+	sl_shape2_draw(positions, count);
 }
 
 void 
 rvg_triangle_strip(const float* positions, int count) {
-	sl_shape_type(TYPE_TRIANGLE_STRIP);
-	sl_shape_draw(positions, count);
+	sl_shape2_type(TYPE_TRIANGLE_STRIP);
+	sl_shape2_draw(positions, count);
 }
 
 void 
 rvg_rect(float xmin, float ymin, float xmax, float ymax, bool filling) {
 	if (filling) {
-		sl_shape_type(TYPE_QUADS);
+		sl_shape2_type(TYPE_QUADS);
 	} else {
-		sl_shape_type(TYPE_LINE_STRIP);
-		sl_shape_draw_node(xmin, ymin, true);
+		sl_shape2_type(TYPE_LINE_STRIP);
+		sl_shape2_draw_node(xmin, ymin, true);
 	}
 
  	float coords[8];
@@ -85,11 +85,11 @@ rvg_rect(float xmin, float ymin, float xmax, float ymax, bool filling) {
  	coords[2] = xmax; coords[3] = ymin;
  	coords[4] = xmax; coords[5] = ymax;
  	coords[6] = xmin; coords[7] = ymax;
- 	sl_shape_draw(coords, 4);
+ 	sl_shape2_draw(coords, 4);
 
 	if (!filling) {
-		sl_shape_draw_node(xmin, ymin, false);
-		sl_shape_draw_node(xmin, ymin, true);
+		sl_shape2_draw_node(xmin, ymin, false);
+		sl_shape2_draw_node(xmin, ymin, true);
 	}
 }
 
@@ -98,8 +98,8 @@ rvg_circle(float x, float y, float radius, bool filling, int segments) {
 	const float k_increment = 2.0f * PI / segments;
 	float theta = 0.0f;
 	if (!filling) {
-		sl_shape_type(TYPE_LINE_STRIP);
-		sl_shape_draw_node(x + radius, y, true);
+		sl_shape2_type(TYPE_LINE_STRIP);
+		sl_shape2_draw_node(x + radius, y, true);
 		float coords[segments * 2 + 2];
 		int ptr = 0;
 		for (int i = 0; i <= segments; ++i) {
@@ -107,13 +107,13 @@ rvg_circle(float x, float y, float radius, bool filling, int segments) {
 			coords[ptr++] = y + sinf(theta) * radius;
 			theta += k_increment;
 		}
-		sl_shape_draw(coords, segments + 1);
-		sl_shape_draw_node(x + radius, y, true);
+		sl_shape2_draw(coords, segments + 1);
+		sl_shape2_draw_node(x + radius, y, true);
 	} else {
-		sl_shape_type(TYPE_TRIANGLE_STRIP);
+		sl_shape2_type(TYPE_TRIANGLE_STRIP);
 		int ptr = 0;
-		sl_shape_draw_node(x, y, true);
-		sl_shape_draw_node(x, y, true);
+		sl_shape2_draw_node(x, y, true);
+		sl_shape2_draw_node(x, y, true);
 		if (segments & 0x1) {
 			float coords[segments * 3 + 3];
 			for (int i = 0; i < segments; i += 2) {
@@ -126,7 +126,7 @@ rvg_circle(float x, float y, float radius, bool filling, int segments) {
 				coords[ptr++] = y + sinf(theta) * radius;
 				theta += k_increment;
 			}
-			sl_shape_draw(coords, (segments * 3 + 3) / 2);
+			sl_shape2_draw(coords, (segments * 3 + 3) / 2);
 		} else {
 			float coords[segments * 3 + 4];
 			for (int i = 0; i < segments; i += 2) {
@@ -143,9 +143,9 @@ rvg_circle(float x, float y, float radius, bool filling, int segments) {
 			coords[ptr++] = y;
 			coords[ptr++] = x + radius;
 			coords[ptr++] = y;
-			sl_shape_draw(coords, segments * 3 / 2 + 2);
+			sl_shape2_draw(coords, segments * 3 / 2 + 2);
 		}
-		sl_shape_draw_node(x + radius, y, true);
-		sl_shape_draw_node(x + radius, y, true);
+		sl_shape2_draw_node(x + radius, y, true);
+		sl_shape2_draw_node(x + radius, y, true);
 	}
 }
--- rvg_render3.h
+++ rvg_render3.h
@@ -1,28 +1,28 @@
-//#ifdef __cplusplus
-//extern "C"
-//{
-//#endif
-//
-//#ifndef rapid_vector_graphics_render3_h
-//#define rapid_vector_graphics_render3_h
-//
-//#include <stdbool.h>
-//
-//void rvg_point3(float x, float y, float z);
-//
-//void rvg_line3(float x0, float y0, float z0, float x1, float y1, float z1);
-//void rvg_lines3(const float* positions, int count);
-//void rvg_polyline3(const float* positions, int count, bool loop);
-//
-//void rvg_triangles3(const float* positions, int count);
-//void rvg_triangle_strip3(const float* positions, int count);
-//
-//void rvg_rect3(float xmin, float ymin, float xmax, float ymax, float z, bool filling);
-//
-//void rvg_circle3(float x, float y, float z, float radius, bool filling, int segments);
-//
-//#endif // rapid_vector_graphics_render3_h
-//
-//#ifdef __cplusplus
-//}
-//#endif
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef rapid_vector_graphics_render3_h
+#define rapid_vector_graphics_render3_h
+
+#include <stdbool.h>
+
+void rvg_point3(float x, float y, float z);
+
+void rvg_line3(float x0, float y0, float z0, float x1, float y1, float z1);
+void rvg_lines3(const float* positions, int count);
+void rvg_polyline3(const float* positions, int count, bool loop);
+
+void rvg_triangles3(const float* positions, int count);
+void rvg_triangle_strip3(const float* positions, int count);
+
+void rvg_rect3(float xmin, float ymin, float xmax, float ymax, float z, bool filling);
+
+void rvg_circle3(float x, float y, float z, float radius, bool filling, int segments);
+
+#endif // rapid_vector_graphics_render3_h
+
+#ifdef __cplusplus
+}
+#endif
