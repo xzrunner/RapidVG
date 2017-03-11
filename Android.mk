67efc4da668f6dfe4dcb81c7773e6f6262dab767
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:20:19 +0800

    build android

--- Android.mk
+++ Android.mk
@@ -11,9 +11,14 @@ LOCAL_CFLAGS := -std=gnu99
 LOCAL_C_INCLUDES  := \
 	${SHADERLAB_SRC_PATH} \
 	${EJOY2D_SRC_PATH} \
+	${SHADERLAB_SRC_PATH}/include \
+	${UNIRENDER_SRC_PATH}/include \
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.c" -print)) \
+
+LOCAL_STATIC_LIBRARIES := \
+	unirender \
 
 include $(BUILD_STATIC_LIBRARY)	
 
